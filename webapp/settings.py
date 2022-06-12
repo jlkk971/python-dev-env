afdabcfd9c8f17e354173d9fca6d3161faf68686
Eva Albers <ehhalbers@gmail.com>
2022-06-12 16:40:40 +0200

    removed secret key from settings

--- webapp/settings.py
+++ webapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from .secrets import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5kyx@d4sl*oh=3eb-+u^w5fx7yh%_t7xfaq6^3$nqg4-ap!lv7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
